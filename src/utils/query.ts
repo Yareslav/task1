1c4242a3648df8415e302b2630bd5ee49feb65f4
Yareslav <80545801+Yareslav@users.noreply.github.com>
2022-09-20 12:02:26 +0300

    fixed bugs with iterators

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -2,7 +2,7 @@ function query(selector: string, isPluralPossible: true): Element[];
 function query(selector: string, isPluralPossible: false): Element | null;
 
 function query(selector: string, isPluralPossible: boolean): Element | Element[] | null {
-  const domElements = document.querySelectorAll(selector);
+  const domElements = Array.from(document.querySelectorAll(selector));
   if (isPluralPossible) return [...domElements];
   if (domElements.length === 0) return null;
   return domElements[0];
